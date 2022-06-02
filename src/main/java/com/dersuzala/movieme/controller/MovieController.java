008094d040849ad1a640cf51c445ef535b120716
Jorge Sanz <jsanz@atsistemas.com>
2022-06-02 10:09:05 +0200

    added docker file

--- src/main/java/com/dersuzala/movieme/controller/MovieController.java
+++ src/main/java/com/dersuzala/movieme/controller/MovieController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MovieController {
 
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "/")
     public String helloWorld(){
         return "Hello";
     }
