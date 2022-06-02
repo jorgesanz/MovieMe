51c7c39d86ad3bdb7e9260908f46ec20f74a7b50
Jorge Sanz <jsanz@atsistemas.com>
2022-06-02 08:34:16 +0200

    spring initializr generation

---
+++ src/main/java/com/desuzala/movieme/MovieMeApplication.java
@@ -0,0 +1,13 @@
+package com.desuzala.movieme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desuzala/movieme/MovieMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desuzala.movieme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
