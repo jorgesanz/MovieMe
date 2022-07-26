1d2d19579acde9c1dcdd3e118ac7cbf74cc606bb
Jorge Sanz <jsanz@atsistemas.com>
2022-07-26 18:38:35 +0200

    added property config

--- src/main/java/com/dersuzala/movieme/controller/movie/MovieController.java
+++ src/main/java/com/dersuzala/movieme/controller/movie/MovieController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController()
@@ -25,11 +26,34 @@ public class MovieController {
         return ResponseEntity.ok(movies);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MovieDto> getMovieById(@PathVariable String id){
+
+        Optional<MovieDto> movie = movieRepository.findById(id);
+
+        return ResponseEntity.of(movie);
+
+    }
+
     @PostMapping
     public ResponseEntity<MovieDto> postMovie(@RequestBody MovieDto movieDto){
         movieDto.setId(UUID.randomUUID().toString());
         movieRepository.save(movieDto);
         return ResponseEntity.ok(movieDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<MovieDto> putMovie(@RequestBody MovieDto movieDto){
+        movieRepository.save(movieDto);
+        return ResponseEntity.ok(movieDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id){
+
+        movieRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+
     }
 
 }
